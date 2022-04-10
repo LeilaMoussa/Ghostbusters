974ded2f1d7bad27b3815543660475c6aa466006
khaoulaSoussi <k.aitsoussi@aui.ma>
2022-04-10 16:40:15 +0000

    Added text display for tiles on mouse click

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,20 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using CodeMonkey.Utils;
 
 public class Tile : MonoBehaviour{
 	[SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
-    // [SerializeField] public TextMeshPro prob;
-    //public GameObject uiSceneText;
-    //public TextMeshProUGUI textMeshProUGUI;
-    // [SerializeField] private Color _clickColor;
-
  
     public void Init(bool isOffset) {
         _renderer.color = isOffset ? _offsetColor : _baseColor;
-        // prob = GetComponent<TextMeshPro>();
     }
  
     void OnMouseEnter() {
@@ -31,8 +26,7 @@ public class Tile : MonoBehaviour{
         // Change the color based on the probabilities
         _highlight.SetActive(false);
         _renderer.color = Color.yellow;
-        //uiSceneText.SetActive(true);
-        //textMeshProUGUI.text = "0.1";
-        // _renderer.material.SetColor("_Color", Color.red);
+        TextMesh text = UtilsClass.CreateWorldText("0.01", null, _renderer.transform.position, 40, Color.black, TextAnchor.MiddleCenter);
+        text.transform.localScale = new Vector3((float)0.1,(float)0.1,(float)0.1);
     }
 }
