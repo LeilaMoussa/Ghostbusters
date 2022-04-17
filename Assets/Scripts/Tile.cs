345963662ad8b40c25e59f4c22277b94d863fc27
Khaoula Ait Soussi <85248282+KhaoulaSoussi@users.noreply.github.com>
2022-04-17 01:44:40 +0000

    Added SetProba() function
    SetProba() gets the probability, converts it to a percentage. If the previous probability is not null, that means we're trying to update, so it'll just override the text as you suggested... Otherwise, the proba will be initialized (i.e., TextMesh obj will be created)

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,30 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using CodeMonkey.Utils;
+using UnityEngine.EventSystems;
+using static Game.GamePlay;
 
-public class Tile : MonoBehaviour{
+public class Tile : MonoBehaviour{  // , IPointerClickHandler
 	[SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
- 
+    [SerializeField] public TextMesh proba = null;
+    private int clickCount = 0;
+
     public void Init(bool isOffset) {
         _renderer.color = isOffset ? _offsetColor : _baseColor;
     }
  
-    void OnMouseEnter() {
+    public void OnMouseEnter() {
         _highlight.SetActive(true);
     }
  
-    void OnMouseExit()
-    {
+    public void OnMouseExit(){
         _highlight.SetActive(false);
     }
 
-    void OnMouseDown(){
+    public void OnMouseDown(){
         // Change the color based on the probabilities
-        _highlight.SetActive(false);
-        _renderer.color = Color.yellow;
-        TextMesh text = UtilsClass.CreateWorldText("0.01", null, _renderer.transform.position, 40, Color.black, TextAnchor.MiddleCenter);
-        text.transform.localScale = new Vector3((float)0.1,(float)0.1,(float)0.1);
+        clickCount++;
+        if(clickCount == 1){
+            _highlight.SetActive(false);
+            _renderer.color = Color.yellow;
+            // Here we should get the probability to be displayed, then send it to SetProba()
+            SetProba(0.5);
+            // UpdatePosteriorProbabilities(_renderer.transform.position, this);
+        }
+        else if(clickCount == 2){
+            Debug.Log("You busted the position " + _renderer.transform.position);
+            _renderer.color = Color.red;
+        }
+    }
+
+    public void SetProba(double proba){
+        double percent = proba*100;
+        string percentProba = percent + "%";
+        if(this.proba != null){
+            // Update the probability
+            this.proba.text = percentProba;
+        }
+        else{
+            Debug.Log(_renderer.transform.position);
+            // Initialize the probability
+            this.proba = UtilsClass.CreateWorldText(percentProba, null, _renderer.transform.position, 30, Color.black, TextAnchor.MiddleCenter);
+            this.proba.transform.localScale = new Vector3((float)0.1,(float)0.1,(float)0.1);
+        }
     }
 }
