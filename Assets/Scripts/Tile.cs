9b4cc46b488078be285071a93a26594d0b7c579d
Khaoula Ait Soussi <85248282+KhaoulaSoussi@users.noreply.github.com>
2022-04-19 01:21:05 +0000

    Update Tile.cs

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,6 +5,7 @@ using TMPro;
 using CodeMonkey.Utils;
 using UnityEngine.EventSystems;
 using static Game.GamePlay;
+using static Game.Globals;
 
 public class Tile : MonoBehaviour{  // , IPointerClickHandler
 	[SerializeField] private Color _baseColor, _offsetColor;
@@ -31,19 +32,26 @@ public class Tile : MonoBehaviour{  // , IPointerClickHandler
         if(clickCount == 1){
             _highlight.SetActive(false);
             _renderer.color = Color.yellow;
-            // Here we should get the probability to be displayed, then send it to SetProba()
-            SetProba(0.5);
-            // UpdatePosteriorProbabilities(_renderer.transform.position, this);
+            Debug.Log(_renderer.transform.position);
+            UpdatePosteriorProbabilities(_renderer.transform.position, this);
         }
         else if(clickCount == 2){
             Debug.Log("You busted the position " + _renderer.transform.position);
-            _renderer.color = Color.red;
+            bool res = Bust(_renderer.transform.position);
+            if(res){
+                _renderer.color = Color.red;
+                Debug.Log("Congrats! You busted the ghost");
+            }
+            else{
+                Debug.Log("Game over!");
+            }
+            Application.Quit();
         }
     }
 
     public void SetProba(double proba){
         double percent = proba*100;
-        string percentProba = percent + "%";
+        string percentProba = percent.ToString("0.000") + "%";
         if(this.proba != null){
             // Update the probability
             this.proba.text = percentProba;
@@ -51,8 +59,25 @@ public class Tile : MonoBehaviour{  // , IPointerClickHandler
         else{
             Debug.Log(_renderer.transform.position);
             // Initialize the probability
-            this.proba = UtilsClass.CreateWorldText(percentProba, null, _renderer.transform.position, 30, Color.black, TextAnchor.MiddleCenter);
+            this.proba = UtilsClass.CreateWorldText(percentProba, null, _renderer.transform.position, 25, Color.black, TextAnchor.MiddleCenter);
             this.proba.transform.localScale = new Vector3((float)0.1,(float)0.1,(float)0.1);
+        }
+    }
+
+    public void SetColor(DisplayedColor c){
+        switch(c){
+            case DisplayedColor.Red:
+                _renderer.color = Color.red;
+                break;
+            case DisplayedColor.Yellow:
+                _renderer.color = Color.yellow;
+                break;
+            case DisplayedColor.Green:
+                _renderer.color = Color.green;
+                break;
+            case DisplayedColor.Orange:
+                _renderer.color = new Color(0.95f,0.32f,0.09f,0.99f);
+                break;
         }
     }
 }
